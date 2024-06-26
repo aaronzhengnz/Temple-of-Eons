db34199f7ed889c7365254a6b601b5cb952ac942
Aaron Zheng <21211@burnside.school.nz>
2024-06-26 18:55:48 +1200

    Created Parallax Effect Script

--- Assets/Scripts/ParallaxMenu.cs
+++ Assets/Scripts/ParallaxMenu.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    private float length, newPos, totalDist, dist;
+    private float length, newPos, totalDist, dist, startPos;
     public GameObject cam;
     public float parallaxEffect;
 
     void Start()
     {
+        totalDist = 0;
+        startPos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+        Debug.Log(startPos + length);
     }
 
     void Update()
     {
-        totalDist = totalDist += dist;
-        newPos = transform.position.x;
         dist = Time.deltaTime * parallaxEffect;
+        newPos = transform.position.x + dist;
+        totalDist += dist;
 
-        transform.position = new Vector3(newPos + dist, transform.position.y, transform.position.z);
+        if (totalDist >= length)
+        {
+            Debug.Log("Exceeded Move Limit");
+            totalDist -= length;
+            newPos = startPos + totalDist;
+            Debug.Log(newPos);
+        }
+
+        transform.position = new Vector3(newPos, transform.position.y, transform.position.z);
     }
 }

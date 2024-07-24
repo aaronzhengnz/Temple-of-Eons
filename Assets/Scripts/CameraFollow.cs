2dc420c53e3b2a3223d76cd60205e0548b349770
Aaron Zheng <21211@burnside.school.nz>
2024-07-24 14:26:13 +1200

    Camera Follow Character (No Drag)

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject Character;
+    private Vector3 updatePosition = Vector3.zero;
+    private Vector3 currentPosition = Vector3.zero;
+
+    private float followDrag;
+    private void Start()
+    {
+        currentPosition = transform.position;
+    }
+    private void Update()
+    {
+        updatePosition.x = Character.transform.position.x - 3.1f;
+        updatePosition.z = Character.transform.position.z;
+        currentPosition = new Vector3(updatePosition.x, currentPosition.y, updatePosition.z);
+        transform.position = currentPosition;
+    }
+}

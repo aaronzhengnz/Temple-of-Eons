93ee8e5c77478213ea25161d6037e8177c18e44d
Aaron Zheng <21211@burnside.school.nz>
2024-07-26 13:17:50 +1200

    Get GameObject Time Warps for Time warps script

--- Assets/Scripts/Level/TimeWarp.cs
+++ Assets/Scripts/Level/TimeWarp.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TimeWarp : MonoBehaviour
 {
+
+    public GameObject PastTimeWarp;
+    public GameObject PresentTimeWarp;
+    public GameObject FutureTimeWarp;
+
     // Start is called before the first frame update
     void Start()
     {

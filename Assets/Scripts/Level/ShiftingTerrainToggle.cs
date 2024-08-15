4b3fd7aad2e67e740be65e0739fab59439ea179d
Aaron Zheng <21211@burnside.school.nz>
2024-08-15 14:28:38 +1200

    Added Toggle script to all shifting terrain and made barrier toggle

--- Assets/Scripts/Level/ShiftingTerrainToggle.cs
+++ Assets/Scripts/Level/ShiftingTerrainToggle.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class ShiftingTerrainToggle : MonoBehaviour
 {
+
+    private GameObject Terrain;
+    private GameObject Barrier;
+    public bool InitialTerrainActive;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Terrain = GameObject.Find("Terrain");
+        Barrier = GameObject.Find("Barrier");
+
+        if (Terrain == null)
+        {
+            Debug.LogError("Terrain GameObject not found");
+        }
+
+        if (Barrier == null)
+        {
+            Debug.LogError("Barrier GameObject not found");
+        }
+
+        // Set initial state based on InitialTerrainActive
+        ToggleTerrainState();
     }
 
-    // Update is called once per frame
-    void Update()
+    void ToggleTerrainState()
     {
-        
+        if (Terrain != null && Barrier != null)
+        {
+            if (InitialTerrainActive)
+            {
+                Debug.Log("Setting Terrain active and Barrier inactive");
+                Terrain.SetActive(true);
+                Barrier.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("Setting Terrain inactive and Barrier active");
+                Terrain.SetActive(false);
+                Barrier.SetActive(true);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        ToggleTerrainState();
     }
 }

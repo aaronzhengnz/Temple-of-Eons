ef03f56830644967540947bd1b4cd29bea823aff
Aaron Zheng <21211@burnside.school.nz>
2024-08-16 14:01:48 +1200

    Fixed ShiftingTerrain Toggle script which enables and disables barrier

--- Assets/Scripts/Level/ShiftingTerrainToggle.cs
+++ Assets/Scripts/Level/ShiftingTerrainToggle.cs
@@ -1,55 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ShiftingTerrainToggle : MonoBehaviour
 {
-
-    private GameObject Terrain;
-    private GameObject Barrier;
+    private GameObject TerrainObject;
+    private GameObject BarrierObject;
     public bool InitialTerrainActive;
 
     // Start is called before the first frame update
     void Start()
     {
-        Terrain = GameObject.Find("Terrain");
-        Barrier = GameObject.Find("Barrier");
-
-        if (Terrain == null)
-        {
-            Debug.LogError("Terrain GameObject not found");
-        }
-
-        if (Barrier == null)
-        {
-            Debug.LogError("Barrier GameObject not found");
-        }
-
-        // Set initial state based on InitialTerrainActive
+        TerrainObject = this.gameObject.transform.Find("Terrain").gameObject;
+        BarrierObject = this.gameObject.transform.Find("Barrier").gameObject;
+    }
+    private void Update()
+    {
         ToggleTerrainState();
     }
 
     void ToggleTerrainState()
     {
-        if (Terrain != null && Barrier != null)
+        if (TerrainObject != null && BarrierObject != null)
         {
             if (InitialTerrainActive)
             {
-                Debug.Log("Setting Terrain active and Barrier inactive");
-                Terrain.SetActive(true);
-                Barrier.SetActive(false);
+                TerrainObject.SetActive(true);
+                BarrierObject.SetActive(false);
             }
             else
             {
-                Debug.Log("Setting Terrain inactive and Barrier active");
-                Terrain.SetActive(false);
-                Barrier.SetActive(true);
+                TerrainObject.SetActive(false);
+                BarrierObject.SetActive(true);
             }
         }
-    }
-
-    private void Update()
-    {
-        ToggleTerrainState();
     }
 }
